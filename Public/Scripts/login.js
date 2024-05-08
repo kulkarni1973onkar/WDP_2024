6ac21170c29332ecc34c046534d6862d9836665d
Onkar Kulkarni <91174083+kulkarni1973onkar@users.noreply.github.com>
2024-05-09 02:49:45 +0530

    feature working

--- Public/Scripts/login.js
+++ Public/Scripts/login.js
@@ -1,24 +1,90 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let logform = document.getElementById("logform");
+let regform = document.getElementById("regform")
 
-    logform.addEventListener('submit', register);
+if(regform) regform.addEventListener('submit', register)
 
-    function register(e) {
-        e.preventDefault();
+function register(e) {
+  e.preventDefault()
 
-        let username = document.getElementById("username").value;
-        let password = document.getElementById("password").value;
-        let email = document.getElementById("email").value;
+  let fname = document.getElementById("fname").value
+  let lastname = document.getElementById("lname").value
+  let email = document.getElementById("email").value
+  let username = document.getElementById("username").value
+  let password = document.getElementById("password").value
+  let RPassword = document.getElementById("RPassword").value
+  
 
+  if (password !== RPassword) {
+    document.querySelector(".error").innerText = "Passwords Must Match!"
+    document.getElementById("password").value = ""
+    document.getElementById("RPassword").value = ""
+  } else {
+    const user = {
+      Username: username,
+      Password: password,
+      Email: email
     }
+    
+    fetchData('/users/register', user, 'POST')
+      .then(data => {
+        if (!data.message) {
+          setCurrentUser(data)
+          window.location.href = "login.html"
+        }
+      })
+      .catch(err => {
+        let error = document.querySelector(".error")
+        error.innerHTML = `${err.message}`
+      })
 
-    // code to add event listener to login form
-    function login(e) {
-        // your code to read in login form
-    }
+    document.getElementById("welcome").innerText = `Welcome ${username}!`
+  }
 
-    // code to add event listener to third form of some kind
-    function someOtherFunction(e) {
-        // your code to read in your third form
-    }
-});
+}
+
+let logform = document.getElementById("logform")
+
+if(logform) logform.addEventListener('submit', login)
+
+function login(e) {
+  e.preventDefault()
+
+  let username = document.getElementById("username").value
+  let password = document.getElementById("password").value
+  let email = document.getElementById("email").value
+  
+
+
+  const user = {
+    Username: username,
+    Password: password
+  }
+  // make a call to the server
+  fetchData('/users/login', user, 'POST')
+    .then(data => {
+      if (!data.message) {
+        setCurrentUser(data)
+        window.location.href = "login.html"
+      }
+    })
+    .catch(err => {
+      let error = document.querySelector(".error")
+      error.innerHTML = `${err.message}`
+    })
+
+  document.getElementById("welcome").innerText = `Welcome ${username}!`
+}
+
+
+// configuring local storage
+function setCurrentUser(user) {
+  localStorage.setItem('user', JSON.stringify(user))
+}
+
+function getCurrentUser() {
+  return JSON.parse(localStorage.getItem('user'))
+}
+
+function removeUser() {
+  localStorage.removeItem('user')
+  window.location.href = 'login.html'
+}
